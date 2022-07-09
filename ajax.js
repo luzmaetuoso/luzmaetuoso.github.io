e71045967888da342a83db9d23c0ea7d407dcd23
Luzmae Jen Tuoso <77155099+luzmaetuoso@users.noreply.github.com>
2022-07-10 03:18:00 +0800

    Delete ajax.js

--- ajax.js
+++
@@ -1,7 +0,0 @@
-$.get('header-menu.html', {}, function (response) {
-    $('div#nav').append(response);
-});
-
-$.get('footer.html', {}, function (response) {
-    $('div#footer').append(response);
-});
